2db44af0fc10f71932ef4f161f1df8d25f37d1d9
Hassan Moussa <hassanmoussa61@gmail.com>
2023-08-06 14:15:51 +0300

    updated styles for contact details

--- contact-manager-fe/src/pages/ContactDetails.jsx
+++ contact-manager-fe/src/pages/ContactDetails.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import axios from "axios";
 import Cookies from "js-cookie";
-
+import "./ContactDetails.css";
 const ContactDetails = () => {
   const { id } = useParams();
 
@@ -20,7 +20,7 @@ const ContactDetails = () => {
   }, []);
 
   return (
-    <div>
+    <div className="containerDetails">
       <div className="contactDetails">
         <h2>{contact.name}</h2>
         <p>Phone: {contact.phone_number}</p>
